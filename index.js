f63ab9d923ccf788e9c44e7ce6f4532757738800
oldlam <oldlam1117@hotmail.com>
2016-07-14 17:40:42 +0800

    Support div

--- index.js
+++ index.js
@@ -12,9 +12,10 @@
 var isIE = /msie/gi.test(navigator.userAgent);
 
 
-function getScrollPos() {
-  // Handle scroll position in case of IE differently
-  if (isIE) {
+function getScrollPos(target) {
+  if (target) {
+    return target.scrollTop;
+  } else if (isIE) { // Handle scroll position in case of IE differently
     return document.documentElement.scrollTop;
   } else {
     return window.pageYOffset;
@@ -28,6 +29,7 @@ function getScrollPos() {
  * @param {Object} options
  *   @param {Number} distance, distance to bottom
  *   @param {Function} callback(done)
+ *   @param {DomElement} target, eg. a scrollable div
  */
 
 function InfiniteScroll(options) {
@@ -39,13 +41,15 @@ function InfiniteScroll(options) {
   options.callback = options.callback || function() {};
   options.distance = options.distance || 50;
   
+  var target = options.target || window;
+  
   this.updateInitiated = false;
-  this.prevScrollPos = getScrollPos();
+  this.prevScrollPos = getScrollPos(target);
   this.options = options;
   this.enabled = false;
   
-  window.addEventListener('scroll',    this._onScroll.bind(this), false);
-  window.addEventListener('touchmove', this._onScroll.bind(this), false);
+  target.addEventListener('scroll',    this._onScroll.bind(this), false);
+  target.addEventListener('touchmove', this._onScroll.bind(this), false);
 }
 
 
@@ -59,7 +63,8 @@ InfiniteScroll.prototype._onScroll = function(e) {
   if (!this.enabled) return;
   if (this.updateInitiated) return;
 
-  var scrollPos = getScrollPos();
+  var target = this.options.target;
+  var scrollPos = getScrollPos(target);
 
   if (scrollPos === this.prevScrollPos) return;
   
@@ -77,6 +82,11 @@ InfiniteScroll.prototype._onScroll = function(e) {
     html.scrollHeight, 
     html.offsetHeight
   );
+  
+  if (target) {
+    clientHeight = target.clientHeight;
+    pageHeight = target.scrollHeight;
+  }
   
   // Check if scroll bar position is just `distance` above the max, if yes, initiate an update
   if (pageHeight - (scrollPos + clientHeight) < this.options.distance) {
